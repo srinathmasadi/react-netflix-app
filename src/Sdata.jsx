76df804f85d0df106c2b748a6266d49da9fc1aec
Masadi Srinath <srinathmasadi@gmail.com>
2021-11-20 15:12:37 +0530

    First Commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,40 @@
+const Sdata = [
+    {
+        imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+        title:"A Netflix Original Series",
+        sname:"DARK",
+        link:"https://netflix.com/in/title/80990668?source=35"
+    },
+    {
+        imgsrc:"https://wallpapercave.com/w/wp9920335.jpg",
+        title:"A Netflix Original Series",
+        sname:"SQUID GAMES",
+        link:"https://netflix.com/in/title/80990668?source=35"
+    },
+    {
+        imgsrc:"https://wallpapercave.com/w/wp9731625.jpg",
+        title:"A Netflix Original Series",
+        sname:"WAR",
+        link:"https://netflix.com/in/title/80990668?source=35"
+    },
+    {
+        imgsrc:"https://wallpapercave.com/w/wp4539233.jpg",
+        title:"A Netflix Original Series",
+        sname:"SPIDER MAN",
+        link:"https://netflix.com/in/title/80990668?source=35"
+    },
+    {
+        imgsrc:"https://wallpapercave.com/w/wp10026739.jpg",
+        title:"A Netflix Original Series",
+        sname:"AVENGERS",
+        link:"https://netflix.com/in/title/80990668?source=35"
+    },
+    {
+        imgsrc:"https://wallpapercave.com/w/wp8698425.jpg",
+        title:"A Netflix Original Series",
+        sname:"SUPERMAN",
+        link:"https://netflix.com/in/title/80990668?source=35"
+    }
+]
+
+export default Sdata;
